68a00e10a2a2a64268cadacc2f8494726a663ea5
skalidindi <skalidindi@vmware.com>
2016-02-15 14:35:08 -0800

    Removed some comments

--- dropdown/src/app.jsx
+++ dropdown/src/app.jsx
@@ -4,29 +4,6 @@ import ReactDOM from 'react-dom';
 import Dropdown from './dropdown';
 import '../index.html';
 
-// thumbnail
-// var options = {
-//    thumbnailData: [
-//       {
-//          title: 'Show Courses',
-//          number: 32,
-//          header: 'Learn React',
-//          description: 'React is a fantastic new library for rendering web pages.',
-//          imageUrl: 'http://formatjs.io/img/react.svg'
-//       },
-//       {
-//          title: 'Show Courses',
-//          number: 25,
-//          header: 'Learn Gulp',
-//          description: 'Gulp will help you speed up your development workflow.',
-//          imageUrl: 'http://brunch.io/images/others/gulp.png'
-//       }
-//    ]
-// };
-
-// var element = React.createElement(ThumbnailList, options);
-// ReactDOM.render(element, document.querySelector('.container'));
-
 // dropdown
 const options = {
   title: 'Choose a desert',
