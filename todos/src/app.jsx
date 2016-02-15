f68548d3cdd21fb885b1ff83c70305df5555ecbd
skalidindi <skalidindi@vmware.com>
2016-02-15 15:21:22 -0800

    Added sass support to todos app and some cleanup code.

--- todos/src/app.jsx
+++ todos/src/app.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../index.html';
+import '../sass/style';
 
 class Hello extends React.Component {
    render() {
-     return <h1 className='red'>
+     return (<h1 className={`red`}>
        Hello!
-     </h1>
+     </h1>);
    }
 }
 
-let element = React.createElement(Hello, {});
+const element = React.createElement(Hello, {});
 ReactDOM.render(element, document.querySelector('.container'));
